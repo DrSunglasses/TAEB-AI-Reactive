a85bd3782e1f9ee16b28a662c8c1f71d634045c8
Jeffrey Bosboom <jbosboom@uci.edu>
2009-06-17 13:27:22 -0700

    Update the currently string based on another analyzer return value

--- lib/TAEB/AI/Reactive.pm
+++ lib/TAEB/AI/Reactive.pm
@@ -29,7 +29,7 @@ use Module::Pluggable (
 sub BUILD {
     my $self = shift;
     TAEB->log->ai("In BUILD, about to add analyzers");
-    for my $analyzer (__PACKAGE__->load_analyzers) {
+    for my $analyzer (load_analyzers()) {
         TAEB->log->ai("Adding analyzer $analyzer").
         $self->add_analyzer($analyzer);
     }
@@ -40,19 +40,23 @@ sub next_action {
     
     my $next_action = undef;
     $self->_set_current_priority(0);
+    my $currently = undef;
     
     TAEB->log->ai("Beginning analyzer loop");
     
     for my $analyzer ($self->analyzers) {
-        my ($action, $priority) = $analyzer->analyze();
+        my ($action, $priority, $currently_string) = $analyzer->analyze();
         TAEB->log->ai("$analyzer proposed $action with priority $priority");
         next unless defined $action;
         
         if ($priority > $self->current_priority) {
             $self->_set_current_priority($priority);
             $next_action = $action;
+            $currently = $currently_string;
         }
     }
+    
+    TAEB->ai->currently("$currently: " . $self->current_priority);
     
     return $next_action;
 }
