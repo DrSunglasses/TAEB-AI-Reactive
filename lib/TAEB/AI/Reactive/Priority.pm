332b429039bbcb546c35dd683fb368a49fddb4f0
Jeffrey Bosboom <jbosboom@uci.edu>
2009-06-19 16:19:32 -0700

    Priorities now use saiph's priority ordering system

--- lib/TAEB/AI/Reactive/Priority.pm
+++ lib/TAEB/AI/Reactive/Priority.pm
@@ -1,13 +1,34 @@
 package TAEB::AI::Reactive::Priority;
 
+# The general priority structure is based on the priority system of
+# saiph, a rival NetHack bot that Jeffrey Bosboom (the author of
+# Reactive) was involved with.
+
 use constant {
-    EAT_STARVING => 955,
-    PRAY_STARVING => 950,
-    EAT_WEAK => 850,
-    EAT_HUNGRY => 500,
-    CURE_USING_UNIHORN => 700,
-    CHECK_INTERESTING => 80,
+    # >= 1000: actions that do not take time
+    # there shouldn't be many of these, these are framework's job
+    
+    # 800-999: emergencies, spend any resource necessary to survive
+    CURE_LEATHAL => 995, # sickness, stoning, sliming etc.
+    FIX_HUNGER_STARVING => 950, # eat if possible, otherwise pray
+    # 600-799: caution advised, but don't expend unnecessary resources
+    
+    # 400-599: don't move, but fight if safe, eat, etc.
+    EAT_WEAK => 590,
+    APPLY_UNIHORN_NONLEATHAL => 580, # hallu, conf, etc.
+    EAT_CURE_NONLEATHAL => 575, # spring of wolfsbane, carrot, etc.
+    EAT_HUNGRY => 450,
+    # 200-399: go about our normal business
+    BUC_ID_ITEMS => 350,
+    TAKE_ITEMS => 300,
+    DROP_ITEMS => 290,
+    # 0-199: explore, etc. until we find something else to do
+    CHECK_INTERESTING => 150,
+    OPEN_DOOR => 100,
+    EXPLORE_FIND_STAIRS_UP => 60,
     EXPLORE => 50,
+    EXPLORE_FIND_STAIRS_DOWN => 40,
+    DESCEND => 30,
     RANDOM_WALK_FALLBACK => 5,
 };
 
